d095b6bf113f7531d2f610f202d6a2f2ec301e7d
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-03-23 11:50:34 +0600

    user post api created

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.port || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qvjjrvn.mongodb.net/?appName=Cluster0`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,10 +27,24 @@ async function run() {
         await client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
+
+        // const placesCollection = client.db("Jj-tourism").collection("places");
+        const userCollection = client.db("Jj-tourism").collection("users");
+
+        // users related apis
+        app.post('/user', async(req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+
+
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
