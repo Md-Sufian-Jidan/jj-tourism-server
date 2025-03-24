bc47338537c4e3570564abe2e9c952f75889f227
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-03-24 11:29:28 +0600

    toursit place delete api added

--- index.js
+++ index.js
@@ -100,9 +100,14 @@ async function run() {
             };
             const result = await addTouristPlaceCollection.updateOne(query, update);
             res.send(result);
-            console.log(query, update, result);
         });
 
+        app.delete('/tourist-delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await addTouristPlaceCollection.deleteOne(query);
+            res.send(result);
+        });
 
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
